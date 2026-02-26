e3b09af38dd7c2fb1de38c4cf966fc3b7313abed
Orsuyedukonda151997 <project151997@gmail.com>
2026-02-27 01:01:17 +0530

    Update supabase.js

--- app/supabase.js
+++ app/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
 
+export const supabase = createClient(
+  "https://iflkpailloixhhnibppz.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmbGtwYWlsbG9peGhobmlicHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMjU2MjcsImV4cCI6MjA4NzcwMTYyN30.M-4LQH29OIVrVGW-A0Q6BkhDZVhoSFzjdQddL_ap9xA"
+);
