65ff520755380c4492e0e160df731055108499f4
Orsuyedukonda151997 <project151997@gmail.com>
2026-02-27 01:53:48 +0530

    Update supabase.js

--- app/supabase.js
+++ app/supabase.js
@@ -1,6 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = "https://iflkpailloixhhnibppz.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmbGtwYWlsbG9peGhobmlicHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMjU2MjcsImV4cCI6MjA4NzcwMTYyN30.M-4LQH29OIVrVGW-A0Q6BkhDZVhoSFzjdQddL_ap9xA"
 
 export const supabase = createClient(
-  "https://iflkpailloixhhnibppz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmbGtwYWlsbG9peGhobmlicHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMjU2MjcsImV4cCI6MjA4NzcwMTYyN30.M-4LQH29OIVrVGW-A0Q6BkhDZVhoSFzjdQddL_ap9xA"
-);
+  supabaseUrl,
+  supabaseAnonKey
+)
